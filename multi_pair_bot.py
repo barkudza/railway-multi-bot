4851bb203d27d36a1aa81cc48db0317ff3e28647
mehmet <kashmehmet@gmail.com>
2025-01-17 13:04:33 +0300

    Her işlem için kullanılacak bakiye (dolar) ve kaldıraç oranı değiştirildi

--- multi_pair_bot.py
+++ multi_pair_bot.py
@@ -20,8 +20,8 @@ ALLOWED_PAIRS = ["BTCUSDT", "XRPUSDT", "DOGEUSDT", "SOLUSDT", "SUIUSDT", "1000SH
 ]  # İşlem yapmak istediğiniz çiftler
 
 # Bot Settings
-POSITION_SIZE_USDT = 30  # Her işlem için kullanılacak bakiye (dolar)
-LEVERAGE = 12  # Kaldıraç oranı
+POSITION_SIZE_USDT = 20  # Her işlem için kullanılacak bakiye (dolar)
+LEVERAGE = 10  # Kaldıraç oranı
 
 # Set Logging
 logging.basicConfig(level=logging.INFO)
